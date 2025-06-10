af75db023c171ba76d10fcb70ec3662926ec9877
Rafi123-CMD <rafiulhuda27@gmail.com>
2025-06-10 10:04:02 +0700

    menambahkan seeder dan menambahkan call all seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Uzumaki Naruto',
+            'username' => 'Naruto',
+            'email'  => 'naruto@cendekia.test',
         ]);
+
+        $this->call(CategorySeeder::class);
+        $this->call(PublisherSeeder::class);
     }
 }
