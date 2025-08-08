680fe378227c6c08fa75a48dcd7f7c0bc447c950
Rafi123-CMD <rafiulhuda27@gmail.com>
2025-08-08 23:03:39 +0700

    slicing dashboard and view data

--- resources/js/Components/CardStat.jsx
+++ resources/js/Components/CardStat.jsx
@@ -2,10 +2,10 @@ import { cn } from '@/lib/utils';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 
 export default function CardStat({ data, children }) {
-    const { title, backgroud, className = '', icon: Icon, iconClassName = '' } = data;
+    const { title, background, className = '', icon: Icon, iconClassName = '' } = data;
 
     return (
-        <Card className={cn(backgroud, className)}>
+        <Card className={cn(background, className)}>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                 <CardTitle className="text-sm font-medium">{title}</CardTitle>
                 {Icon && <Icon className={cn('size-5', iconClassName)} />}
